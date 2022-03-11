ad7bd7c2f332a673eb3b059f5abc5c900007e225
ValEmpire <palma_arcival@yahoo.com>
2022-03-11 12:36:48 -0500

    setup navigation and create sign in screen

--- App.js
+++ App.js
@@ -11,57 +11,30 @@
  */
 
 import React from "react";
-import { StyleSheet } from "react-native";
-import {
-  ApplicationProvider,
-  Button,
-  Icon,
-  IconRegistry,
-  Layout,
-  Text,
-} from "@ui-kitten/components";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import * as eva from "@eva-design/eva";
 import { default as theme } from "./src/theme/custom-theme.json";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SignInScreen from "./src/screens/SignInScreen";
 
 /**
  * Use any valid `name` property from eva icons (e.g `github`, or `heart-outline`)
  * https://akveo.github.io/eva-icons
  */
-const HeartIcon = props => <Icon {...props} name="heart" />;
+
+const Auth = createNativeStackNavigator();
 
 export default () => (
   <>
     <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
-      <Layout style={styles.container}>
-        <Text style={styles.text} category="h1">
-          Welcome to UI Kitten 😻
-        </Text>
-        <Text style={styles.text} category="s1">
-          Start with editing App.js to configure your App
-        </Text>
-        <Text style={styles.text} appearance="hint">
-          For example, try changing theme to Dark by using eva.dark
-        </Text>
-        <Button style={styles.likeButton} accessoryLeft={HeartIcon}>
-          LIKE
-        </Button>
-      </Layout>
+      <NavigationContainer>
+        <Auth.Navigator>
+          <Auth.Screen name="SignIn" component={SignInScreen} />
+        </Auth.Navigator>
+      </NavigationContainer>
     </ApplicationProvider>
   </>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  text: {
-    textAlign: "center",
-  },
-  likeButton: {
-    marginVertical: 16,
-  },
-});
